38211637aa336f68da09230b54acd4b9bfc228da
zhangshaofeng <zhangshaofeng@hotmail.com>
2021-06-11 14:41:22 +0800

    spring demo

---
+++ src/main/java/com/tacocloud/TacocloudApplication.java
@@ -0,0 +1,18 @@
+package com.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacocloudApplication {
+
+    public static void main(String[] args) {
+        /**
+        *Spring 主类
+        *@Param
+        *Return
+        */
+        SpringApplication.run(TacocloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tacocloud/TacocloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacocloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
